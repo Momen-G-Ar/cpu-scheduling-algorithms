461bfdd89533c5c4cbf05c62ef77c4652b11220d
Momen-G-Ar-Official <cp.momen@gmail.com>
2023-05-07 06:23:00 +0300

    Setup the PCB

--- PCB.h
+++ PCB.h
@@ -1,11 +1,25 @@
-#ifndef CPU_SCHEDULING_ALGORITHMS_H
-#define CPU_SCHEDULING_ALGORITHMS_H
+#pragma once
 
 using namespace std;
 
-class PCB{
+// This file is the place to build the Process control Block (PCB)
+class PCB
+{
+public:
+    // The Attributes of the PCB class
+    int id;
+    int arrival_time;
+    int cpu_burst;
+    int size_in_bytes;
+    int last_time_in_ready;
     
+    // The methods of the PCB class
+    PCB(int id, int arrival_time, int cpu_burst, int size_in_bytes)
+    {
+        this->id = id;
+        this->arrival_time = arrival_time;
+        this->cpu_burst = cpu_burst;
+        this->size_in_bytes = size_in_bytes;
+        this->last_time_in_ready = arrival_time;
+    }
 };
-
-
-#endif
