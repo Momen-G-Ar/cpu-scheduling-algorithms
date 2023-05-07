0a2bed7643af5adb3f034c26fbd9acad952b0fe7
Momen-G-Ar-Official <cp.momen@gmail.com>
2023-05-07 09:28:22 +0300

    edit the PCB to use it & find the required

--- PCB.h
+++ PCB.h
@@ -11,8 +11,12 @@ public:
     int arrival_time;
     int cpu_burst;
     int size_in_bytes;
+    int finish_time;
+    int waiting_time;
+    int turn_around_time;
+
     int last_time_in_ready;
-    
+
     // The methods of the PCB class
     PCB(int id, int arrival_time, int cpu_burst, int size_in_bytes)
     {
@@ -21,5 +25,8 @@ public:
         this->cpu_burst = cpu_burst;
         this->size_in_bytes = size_in_bytes;
         this->last_time_in_ready = arrival_time;
+        this->finish_time = 0;
+        this->waiting_time = 0;
+        this->turn_around_time = 0;
     }
 };
